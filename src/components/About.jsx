6958c97eab55675c20a0aeb8ccfaaf79c8e689e7
Zoubair AGUNAOU <zoubairagunaou.za@gmail.com>
2026-01-27 20:49:08 +0100

    Final 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,79 +2,69 @@ function About(){
     return (
         <section id="about" className="about-area-2">
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
+                <div className="about-header">
+                    <div className="">
                         <div className="section-title pb-10 wow fadeInUp" data-wow-duration="1.5s"
                              data-wow-delay="0.2s">
-                            <h4 className="title">Services We Provide</h4>
-                            <p className="text">Stop wasting time and money designing and managing a website that
-                                doesn’t get results. Happiness guaranteed!</p>
+                            <h4 className="title">Services que Nous Proposons</h4>
+                            <p className="text">Arrêtez de perdre du temps et de l’argent avec des offres d’énergie complexes.
+                                Chez VERDISSIA, nous vous accompagnons vers une consommation plus simple, plus verte et plus économique. Satisfaction garantie!</p>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-8">
-                        <div className="row">
-                            <div className="col-md-6">
+                <div className="">
+                    <div className="">
+                        <div className="">
+                            <div className="">
                                 <div className="about-content-2 mt-40 d-sm-flex wow fadeInUp" data-wow-duration="1.5s"
                                      data-wow-delay="0.5s">
                                     <div className="about-icon">
                                         <i className="lni-school-compass"></i>
                                     </div>
                                     <div className="about-content media-body">
-                                        <h4 className="about-title">Graphics Design</h4>
-                                        <p className="text">Short description for the ones who look for something
-                                            new.</p>
+                                        <h4 className="about-title">Fourniture d’Énergie Verte</h4>
+                                        <p className="text">Nous proposons des offres d’électricité 100% verte, fiables et adaptées à tous les foyers.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-md-6">
+                            <div className="">
                                 <div className="about-content-2 mt-40 d-sm-flex wow fadeInUp" data-wow-duration="1.5s"
                                      data-wow-delay="0.7s">
                                     <div className="about-icon">
                                         <i className="lni-construction"></i>
                                     </div>
                                     <div className="about-content media-body">
-                                        <h4 className="about-title">Product Design</h4>
-                                        <p className="text">Short description for the ones who look for something
-                                            new.</p>
+                                        <h4 className="about-title">Installation & Mise en Service</h4>
+                                        <p className="text">Nos équipes vous assistent dans chaque étape pour garantir une mise en service rapide et sans stress.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-md-6">
+                            <div className="">
                                 <div className="about-content-2 mt-40 d-sm-flex wow fadeInUp" data-wow-duration="1.5s"
                                      data-wow-delay="0.9s">
                                     <div className="about-icon">
                                         <i className="lni-cup"></i>
                                     </div>
                                     <div className="about-content media-body">
-                                        <h4 className="about-title">UI & UX Design</h4>
-                                        <p className="text">Short description for the ones who look for something
-                                            new.</p>
+                                        <h4 className="about-title">Suivi de Consommation Intelligent</h4>
+                                        <p className="text">Grâce à votre espace client, suivez vos dépenses en temps réel et optimisez votre budget.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-md-6">
+                            <div className="">
                                 <div className="about-content-2 mt-40 d-sm-flex wow fadeInUp" data-wow-duration="1.5s"
                                      data-wow-delay="1.2s">
                                     <div className="about-icon">
                                         <i className="lni-laptop-phone"></i>
                                     </div>
                                     <div className="about-content media-body">
-                                        <h4 className="about-title">App Development</h4>
-                                        <p className="text">Short description for the ones who look for something
-                                            new.</p>
+                                        <h4 className="about-title">Conseils en Optimisation Énergétique</h4>
+                                        <p className="text">Nos experts analysent votre consommation et vous proposent des solutions pour réduire votre facture durablement.</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="about-image d-lg-flex align-items-center">
-                <div className="image wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="0.6s"
-                     data-wow-offset="50">
-                    <img src="/template-assets/images/about-2.png" alt="about"/>
                 </div>
             </div>
         </section>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import About from "../components/About.jsx";
-import Blog from "../components/Blog.jsx";
 import Hero from "../components/Hero.jsx";
 
 function HomePage(){
@@ -7,7 +6,6 @@ function HomePage(){
         <>
             <Hero />
             <About />
-            <Blog />
             <a href="#" className="back-to-top"><i className="lni-chevron-up"></i></a>
         </>
     )
