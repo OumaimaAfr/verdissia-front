7466b66f0d77b4631bc2aafb917a1d6fcb755f61
Zoubair AGUNAOU <zoubairagunaou.za@gmail.com>
2026-01-28 17:00:25 +0100

    Final 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,41 +6,41 @@ function Footer(){
                     <div className="row">
                         <div className="col-lg-3 col-sm-6">
                             <div className="footer-link">
-                                <h6 className="footer-title">Company</h6>
+                                <h6 className="footer-title">Entreprise</h6>
                                 <ul>
-                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">À propos</a></li>
                                     <li><a href="#">Contact</a></li>
-                                    <li><a href="#">Investors</a></li>
+                                    <li><a href="#">Engagements & Valeurs</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-lg-3 col-sm-6">
                             <div className="footer-link">
-                                <h6 className="footer-title">Solutions</h6>
+                                <h6 className="footer-title">Solutions Énergie</h6>
                                 <ul>
-                                    <li><a href="#">Facilities Services</a></li>
-                                    <li><a href="#">Workplace Staffing</a></li>
-                                    <li><a href="#">Project Management</a></li>
+                                    <li><a href="#">Offres d’Électricité Verte</a></li>
+                                    <li><a href="#">Offres Pro</a></li>
+                                    <li><a href="#">Autoconsommation & Solaire</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-lg-3 col-sm-6">
                             <div className="footer-link">
-                                <h6 className="footer-title">Product & Services</h6>
+                                <h6 className="footer-title">Produits & Services</h6>
                                 <ul>
-                                    <li><a href="#">Products</a></li>
-                                    <li><a href="#">Business</a></li>
-                                    <li><a href="#">Developer</a></li>
+                                    <li><a href="#">Suivi de consommation</a></li>
+                                    <li><a href="#">Optimisation énergétique</a></li>
+                                    <li><a href="#">Mise en service</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-lg-3 col-sm-6">
                             <div className="footer-link">
-                                <h6 className="footer-title">Help & Suuport</h6>
+                                <h6 className="footer-title">Aide & Support</h6>
                                 <ul>
-                                    <li><a href="#">Support Center</a></li>
+                                    <li><a href="#">Centre d’aide</a></li>
                                     <li><a href="#">FAQ</a></li>
-                                    <li><a href="#">Terms & Conditions</a></li>
+                                    <li><a href="#">Conditions générales</a></li>
                                 </ul>
                             </div>
                         </div>
--- src/pages/Formulaire/Formulaire.jsx
+++ src/pages/Formulaire/Formulaire.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Steps} from 'antd';
 import { LeftOutlined, ReloadOutlined } from '@ant-design/icons';
-import InfosPersoStep from "./steps/InfosPersoStep.jsx"
+import InfosPersoStep from "./steps/InfosPersoStep.jsx";
 import InfosFournitureStep from "./steps/InfosFournitureStep.jsx";
 import OffresStep from "./steps/OffresStep.jsx";
 import ConfirmationStep from "./steps/ConfirmationStep.jsx";
@@ -18,6 +18,24 @@ function FormulaireInner(){
 
     const { refreshToken } = React.useContext(AuthTokenContext);
 
+    const topRef = React.useRef(null);
+
+    const scrollToTop = React.useCallback((opts = {}) => {
+        const { offset = 0, smooth = true } = opts;
+
+        if (topRef.current) {
+            const elementTop = topRef.current.getBoundingClientRect().top + window.pageYOffset;
+            const target = Math.max(0, elementTop - offset - 90);
+            window.scrollTo({ top: target, behavior: smooth ? 'smooth' : 'auto' });
+        } else {
+            window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
+        }
+    }, []);
+
+    React.useEffect(() => {
+        scrollToTop({ offset: 0, smooth: true });
+    }, [current, isSubmitted, scrollToTop]);
+
     const onFinishInfosPersoStep = (values) => {
         setInfosPersoDetails(values);
         setCurrent(1);
@@ -27,7 +45,7 @@ function FormulaireInner(){
         setinfosFournitureDetails(values);
         setOffres(offresArray || []);
         setCurrent(2);
-    }
+    };
 
     const onValidateOffres = (offre) => {
         setSelectedOffre(offre);
@@ -38,9 +56,8 @@ function FormulaireInner(){
         <InfosPersoStep onFinish={onFinishInfosPersoStep} initialValues={infosPersoDetails} resetSignal={resetCounter}/>,
         <InfosFournitureStep onFinish={onFinishInfosFournitureStep} initialValues={infosFournitureDetails} resetSignal={resetCounter}/>,
         <OffresStep offres={offres || []} onFinish={onValidateOffres} initialValues={{ selectedId: selectedOffre?.id }} resetSignal={resetCounter}/>,
-        <ConfirmationStep infosPerso={infosPersoDetails} infosFourniture={infosFournitureDetails} selectedOffre={selectedOffre} resetSignal={resetCounter} onSubmitted={(ok) => {if (ok) setIsSubmitted(true);}}/>
-    ]
-
+        <ConfirmationStep infosPerso={infosPersoDetails} infosFourniture={infosFournitureDetails} selectedOffre={selectedOffre} resetSignal={resetCounter} onSubmitted={(ok) => { if (ok) setIsSubmitted(true); }}/>
+    ];
 
     const isStepDisabled = (step) => {
         switch (step) {
@@ -79,10 +96,11 @@ function FormulaireInner(){
         setResetCounter((n) => n + 1);
         setIsSubmitted(false);
         refreshToken();
+        scrollToTop({ offset: 0, smooth: true });
     };
 
     return (
-        <section className="form-container">
+        <section className="form-container" ref={topRef}>
             <div className="form">
                 <div className="form-header-buttons">
                     <div className="left-actions">
@@ -124,7 +142,7 @@ function FormulaireInner(){
                 {steps[current]}
             </div>
         </section>
-    )
+    );
 }
 
 
